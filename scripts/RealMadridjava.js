5808c2510fa959dc5efcfbd0328b8242fc5b18d1
jovannsingh <91875010+jovannsingh@users.noreply.github.com>
2022-10-03 19:29:25 +0200

    Add files via upload

--- scripts/RealMadridjava.js
+++ scripts/RealMadridjava.js
@@ -1,14 +1,14 @@
 
 function home(){
-    document.getElementById("imageslider") .src="images/RM_Home.png";
+    document.getElementById("imageslider") .src="/images/RM_Home.png";
 }
 
 function away(){
-    document.getElementById("imageslider") .src="images/RM_Away.png";
+    document.getElementById("imageslider") .src="/images/RM_Away.png";
 }
 
 function third(){
-    document.getElementById("imageslider") .src="images/RM_third.png";
+    document.getElementById("imageslider") .src="/images/RM_Third.png";
 }
 
 document.getElementById("home").addEventListener("click", home);
@@ -16,14 +16,14 @@ document.getElementById("away").addEventListener("click", away);
 document.getElementById("third").addEventListener("click", third);
 
 
-function changeicon(){
-    let displayimage = document.getElementById('ToggleImage')
-        if(displayimage.src.match('/images/brightness-low-fill.svg')){
-            displayimage.src = '/images/moon-stars-fill.svg'
-        } else {
-            displayimage.src = '/images/brightness-low-fill.svg'
-        }
-}   
+// function changeicon(){
+//     let displayimage = document.getElementById('ToggleImage')
+//         if(displayimage.src.match('/images/brightness-low-fill.svg')){
+//             displayimage.src = '/images/moon-stars-fill.svg'
+//         } else {
+//             displayimage.src = '/images/brightness-low-fill.svg'
+//         }
+// }   
 
 
 var menustreepjes = document.querySelector("header button");
